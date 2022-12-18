aaf6684947927ebc0a1bc0bcfec615c0f76da55b
Cristian Butzke <butzke@DESKTOP-JHT4PDR.localdomain>
2022-12-18 15:45:48 -0300

    Add data script

---
+++ script.sql
@@ -0,0 +1,22 @@
+--  Auto-generated SQL script #202212181254
+INSERT INTO laravel.roles (id,description)
+	VALUES (1,'Admin');
+INSERT INTO laravel.roles (id,description)
+	VALUES (2,'Cliente');
+INSERT INTO laravel.roles (id,description)
+	VALUES (3,'Prestador');
+
+--  Auto-generated SQL script #202212181256
+INSERT INTO laravel.categories (id,title,description)
+	VALUES (1,'Jardinagem','Serviços em jardins');
+INSERT INTO laravel.categories (id,title,description)
+	VALUES (2,'Mecânica','Reparos em automóveis');
+INSERT INTO laravel.categories (id,title,description)
+	VALUES (3,'Encanamento','Reparos em canos');
+
+--  Auto-generated SQL script #202212181257
+INSERT INTO laravel.states (id,name,abbreviation)
+	VALUES (1,'São Paulo','SP');
+INSERT INTO laravel.states (id,name,abbreviation)
+	VALUES (2,'Santa Catarina','SC');
+  
